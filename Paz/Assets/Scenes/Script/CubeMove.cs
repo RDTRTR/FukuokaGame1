62085078658d7f7b61834a8a0f3f087a7f8a769e
AnjuzzZZ <56670053+AnjuzzZZ@users.noreply.github.com>
2021-10-12 11:24:38 +0900

    色変更実装

--- Paz/Assets/Scenes/Script/CubeMove.cs
+++ Paz/Assets/Scenes/Script/CubeMove.cs
@@ -3,86 +3,95 @@ using System.Collections;
 
 public class CubeMove : MonoBehaviour
 {
+	Vector3 rotatePoint = Vector3.zero;  //回転の中心
+	Vector3 rotateAxis = Vector3.zero;   //回転軸
+	float cubeAngle = 0f;                //回転角度
 
-	public float rotationPeriod = 0.3f;     // 隣に移動するのにかかる時間
-	public float sideLength = 1f;           // Cubeの辺の長さ
+	float cubeSizeHalf;                  //キューブの大きさの半分
+	bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
 
-	bool isRotate = false;                  // Cubeが回転中かどうかを検出するフラグ
-	float directionX = 0;                   // 回転方向フラグ
-	float directionZ = 0;                   // 回転方向フラグ
-
-	Vector3 startPos;                       // 回転前のCubeの位置
-	float rotationTime = 0;                 // 回転中の時間経過
-	float radius;                           // 重心の軌道半径
-	Quaternion fromRotation;                // 回転前のCubeのクォータニオン
-	Quaternion toRotation;                  // 回転後のCubeのクォータニオン
-
-	// Use this for initialization
 	void Start()
 	{
-
-		// 重心の回転軌道半径を計算
-		radius = sideLength * Mathf.Sqrt(2f) / 2f;
-
+		cubeSizeHalf = transform.localScale.x / 2f;
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
+		//回転中は入力を受け付けない
+		if (isRotate)
+			return;
 
-		float x = 0;
-		float y = 0;
-
-		// キー入力を拾う。
-		x = Input.GetAxisRaw("Horizontal");
-		if (x == 0)
+		if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
-			y = Input.GetAxisRaw("Vertical");
+			
+			rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
+			rotateAxis = new Vector3(0, 0, -1);
 		}
-
-
-		// キー入力がある　かつ　Cubeが回転中でない場合、Cubeを回転する。
-		if ((x != 0 || y != 0) && !isRotate)
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
-			directionX = y;                                                             // 回転方向セット (x,yどちらかは必ず0)
-			directionZ = x;                                                             // 回転方向セット (x,yどちらかは必ず0)
-			startPos = transform.position;                                              // 回転前の座標を保持
-			fromRotation = transform.rotation;                                          // 回転前のクォータニオンを保持
-			transform.Rotate(directionZ * 90, 0, directionX * 90, Space.World);     // 回転方向に90度回転させる
-			toRotation = transform.rotation;                                            // 回転後のクォータニオンを保持
-			transform.rotation = fromRotation;                                          // CubeのRotationを回転前に戻す。（transformのシャローコピーとかできないんだろうか…。）
-			rotationTime = 0;                                                           // 回転中の経過時間を0に。
-			isRotate = true;                                                            // 回転中フラグをたてる。
+			
+			rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
+			rotateAxis = new Vector3(0, 0, 1);
+			
 		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, cubeSizeHalf);
+			rotateAxis = new Vector3(1, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, -cubeSizeHalf);
+			rotateAxis = new Vector3(-1, 0, 0);
+		}
+		// 入力がない時はコルーチンを呼び出さないようにする
+		if (rotatePoint == Vector3.zero)
+			return;
+		StartCoroutine(MoveCube());
 	}
 
-	void FixedUpdate()
+	IEnumerator MoveCube()
 	{
+		//回転中のフラグを立てる
+		isRotate = true;
 
-		if (isRotate)
+		//回転処理
+		float sumAngle = 0f; //angleの合計を保存
+		while (sumAngle < 90f)
 		{
+			cubeAngle = 0.5f; //ここを変えると回転速度が変わる
+			sumAngle += cubeAngle;
 
-			rotationTime += Time.fixedDeltaTime;                                    // 経過時間を増やす
-			float ratio = Mathf.Lerp(0, 1, rotationTime / rotationPeriod);          // 回転の時間に対する今の経過時間の割合
-
-			// 移動
-			float thetaRad = Mathf.Lerp(0, Mathf.PI / 2f, ratio);                   // 回転角をラジアンで。
-			float distanceX = -directionX * radius * (Mathf.Cos(45f * Mathf.Deg2Rad) - Mathf.Cos(45f * Mathf.Deg2Rad + thetaRad));      // X軸の移動距離。 -の符号はキーと移動の向きを合わせるため。
-			float distanceY = radius * (Mathf.Sin(45f * Mathf.Deg2Rad + thetaRad) - Mathf.Sin(45f * Mathf.Deg2Rad));                        // Y軸の移動距離
-			float distanceZ = directionZ * radius * (Mathf.Cos(45f * Mathf.Deg2Rad) - Mathf.Cos(45f * Mathf.Deg2Rad + thetaRad));           // Z軸の移動距離
-			transform.position = new Vector3(startPos.x + distanceX, startPos.y + distanceY, startPos.z + distanceZ);                       // 現在の位置をセット
-
-			// 回転
-			transform.rotation = Quaternion.Lerp(fromRotation, toRotation, ratio);      // Quaternion.Lerpで現在の回転角をセット（なんて便利な関数）
-
-			// 移動・回転終了時に各パラメータを初期化。isRotateフラグを下ろす。
-			if (ratio == 1)
+			// 90度以上回転しないように値を制限
+			if (sumAngle > 90f)
 			{
-				isRotate = false;
-				directionX = 0;
-				directionZ = 0;
-				rotationTime = 0;
+				cubeAngle -= sumAngle - 90f;
 			}
+			transform.RotateAround(rotatePoint, rotateAxis, cubeAngle);
+
+			yield return null;
 		}
+
+		//回転中のフラグを倒す
+		isRotate = false;
+		rotatePoint = Vector3.zero;
+		rotateAxis = Vector3.zero;
+
+		yield break;
+	}
+
+	bool ObjDic(Vector3 DirVec)
+    {
+		RaycastHit hit;
+
+		Physics.Raycast(transform.position, DirVec, out hit);
+		Debug.DrawRay(transform.position, DirVec);
+			
+		if(hit.distance <= 0.8)
+        {
+			return true;
+        }
+
+		return false;
 	}
 }
